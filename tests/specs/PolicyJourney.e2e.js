39f0c4419ac0d6752cf63f71a9b2212ef6bcf6a0
Shiva Raju <shiva.raju@esure.com>
2021-03-18 09:45:25 +0000

    update test

--- tests/specs/PolicyJourney.e2e.js
+++ tests/specs/PolicyJourney.e2e.js
@@ -1,3 +1,4 @@
+
 import App from '../pageobjects/App'
 import BuyPage from '../pageobjects/pages/BuyPage'
 import DrivePage from '../pageobjects/pages/DrivePage'
@@ -6,16 +7,27 @@ import reviewdata from '../pageobjects/Resources/review.page.data'
 import drivedata from '../pageobjects/Resources/drive.page.data'
 const { percySnapshot } = require('@percy/webdriverio')
 
-describe('Policy Journey', () => {
-	it('Buy a Policy', () => {
+describe('Given I am on policy journey', () => {
+	
+	it('Then I can buy  a policy by interacting review, buy and drive pages', () => {
 		App.openHomePage()
-		ReviewPage.checkPNCDText(reviewdata.pncdText)
-		ReviewPage.checkLCoverText(reviewdata.lCoverText)
+		
+		// review page
+		ReviewPage.checkPNCDText('We will protect your 13 years no claims discount')
+		ReviewPage.checkLCoverText('Motor Legal Cover')
 		ReviewPage.clickPayButton()
+		  		
 		// Buy page
 		BuyPage.clickPayNowButton()
+		
 		// drive page
-	   DrivePage.checkPolicyNo(drivedata.policyNo)
-	   DrivePage.checkCoverStartDate(drivedata.coverStartDate)
+	   DrivePage.checkPolicyNo('PL0321789')
+	   DrivePage.checkCoverStartDate('15 December 2020')
+	    
 	})
+
+	it('Run test in different browsers', () => {
+		
+		
+	});
 })
